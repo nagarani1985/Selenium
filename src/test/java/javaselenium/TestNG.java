d3616cbf1bebe253662cf97b7d9405b64faf4190
seshu <seshu@Sahasra>
2022-09-07 10:52:41 -0700

    selenium commit

---
+++ src/test/java/javaselenium/TestNG.java
@@ -0,0 +1,40 @@
+package javaselenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestNG {
+	@Test
+
+	public void googlesearch() throws InterruptedException {
+
+		WebDriver driver;
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		Thread.sleep(3000);
+		driver.get("https://www.google.com/");
+		WebElement searchBox = driver
+				.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+		searchBox.sendKeys("Google text");
+		searchBox.sendKeys(Keys.ENTER);
+		String currentUrl=driver.getCurrentUrl();
+		System.out.println("The current URL is:"+currentUrl);
+		System.out.println("The title is:"+driver.getTitle());
+		driver.navigate().back();
+		Thread.sleep(3000);
+		driver.navigate().forward();
+		Thread.sleep(3000);
+		driver.navigate().refresh();
+		driver.quit();
+		
+		
+		
+		
+	}
+}
